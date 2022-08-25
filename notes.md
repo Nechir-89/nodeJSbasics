c57449b28147710b3acde397408bf869e4b2b34d
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-08-25 11:04:10 -0400

    add typescript to the project

--- notes.md
+++ notes.md
@@ -7,3 +7,8 @@
 - __dirname and __filename are not defined in ES module scope
 - since global variables like __dirname and __filename are not working with type module, I'm returning back to commonJS
 
+- use ES6 module instade of CommonJS modules
+- install typescript with nodejs
+- installing typescript for nodejs also requires installing @types/node
+- to run nodejs in typescript we need to have ts-node-dev package
+- all projects must have src directory for source code.
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+console.log("nodejs is working with typescript");
+// console.log(global);
+
+// import { type, version, homedir, totalmem } from 'os'
+// import {dirname, basename, extname} from 'path'
+// import os from 'os';
+// import path
+
+
+// console.log(type());
+// console.log(version());
+// console.log(homedir());
+// console.log(totalmem());
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// console.log(dirname(__filename));
+// console.log(basename(__filename));
+// console.log(extname(__filename));
+
